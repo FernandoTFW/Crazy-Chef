4fae6e45f9118ba144c6ba2be58b41d7db4c596b
FernandoTFW <fer.aparicio1811@gmail.com>
2022-11-30 22:39:27 -0400

    Efecto humo y movimiento del jugador agregado

--- Assets/01_Scripts/Player.cs
+++ Assets/01_Scripts/Player.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region Movement
+    public Rigidbody rb;
+    public float speed = 7;
+    public float dashForce = 7;
+    bool canPush = true;
+    float dashTime = 1;
+    bool dashing = false;
+    #endregion
+
+    #region timer
+    float timer = 0;
+    #endregion
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,50 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        CheckIfCanDash();
+        Dash();
     }
+
+
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 movementDirection = new Vector3(x, 0, 0);
+        movementDirection.Normalize();
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);//Moverse
+
+    }
+
+    void CheckIfCanDash()
+    {
+        if (!canPush)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 3)
+            {
+                timer = 0;
+                canPush = true;
+            }
+        }
+    }
+
+    void Dash(){
+        if(canPush && Input.GetKeyDown(KeyCode.E)){
+            StartCoroutine(ActivateDash());
+        }
+    }
+
+    IEnumerator ActivateDash()
+    {
+        speed *= 4;
+        canPush = false;
+        dashing = true;
+        yield return new WaitForSeconds(dashTime);
+        speed /= 4;
+        dashing = false;
+    }
+
+
 }
